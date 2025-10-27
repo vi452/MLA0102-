cc939a9ffa1e170c9b7d1ea4003934876c7a413a
RACHAM REDDY VISWESWARA REEDY <192425153.simats@saveetha.com>
2025-10-27 13:39:21 +0530

    Add files via upload

---
+++ Minimax algorithm with Alpha–Beta.py
@@ -0,0 +1,28 @@
+import math
+
+def minimax(depth, node, isMax, values, alpha, beta):
+    if depth == 3:      # Leaf node
+        return values[node]
+    
+    if isMax:
+        best = -math.inf
+        for i in range(2):
+            val = minimax(depth+1, node*2+i, False, values, alpha, beta)
+            best = max(best, val)
+            alpha = max(alpha, best)
+            if beta <= alpha: break  # Prune
+        return best
+    else:
+        best = math.inf
+        for i in range(2):
+            val = minimax(depth+1, node*2+i, True, values, alpha, beta)
+            best = min(best, val)
+            beta = min(beta, best)
+            if beta <= alpha: break  # Prune
+        return best
+
+# Leaf node values (game outcomes)
+values = [3, 5, 6, 9, 1, 2, 0, -1]
+
+print("Best Move Value for AI (Max Player):",
+      minimax(0, 0, True, values, -math.inf, math.inf))
