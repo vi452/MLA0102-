d2c87a6919cb36139fff20871de9f089e304ca04
RACHAM REDDY VISWESWARA REEDY <192425153.simats@saveetha.com>
2025-10-27 13:50:15 +0530

    Update Minimax algorithm with Alpha–Beta.py

--- Minimax algorithm with Alpha–Beta.py
+++ Minimax algorithm with Alpha–Beta.py
@@ -1,16 +1,14 @@
 import math
-
 def minimax(depth, node, isMax, values, alpha, beta):
-    if depth == 3:      # Leaf node
+    if depth == 3:    
         return values[node]
-    
     if isMax:
         best = -math.inf
         for i in range(2):
             val = minimax(depth+1, node*2+i, False, values, alpha, beta)
             best = max(best, val)
             alpha = max(alpha, best)
-            if beta <= alpha: break  # Prune
+            if beta <= alpha: break  
         return best
     else:
         best = math.inf
@@ -18,11 +16,13 @@ def minimax(depth, node, isMax, values, alpha, beta):
             val = minimax(depth+1, node*2+i, True, values, alpha, beta)
             best = min(best, val)
             beta = min(beta, best)
-            if beta <= alpha: break  # Prune
+            if beta <= alpha: break 
         return best
-
-# Leaf node values (game outcomes)
 values = [3, 5, 6, 9, 1, 2, 0, -1]
-
 print("Best Move Value for AI (Max Player):",
       minimax(0, 0, True, values, -math.inf, math.inf))
+
+
+output:
+Best Move Value for AI (Max Player): 5
+
