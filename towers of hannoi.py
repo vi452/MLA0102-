36a2199d5eca47fee6582e8adb992957827b8e44
RACHAM REDDY VISWESWARA REEDY <192425153.simats@saveetha.com>
2025-10-27 13:55:25 +0530

    Update towers of hannoi.py

--- towers of hannoi.py
+++ towers of hannoi.py
@@ -5,8 +5,17 @@ def towers_of_hanoi(n, source, auxiliary, destination):
     towers_of_hanoi(n - 1, source, destination, auxiliary)
     print(f"Move disk {n} from {source} → {destination}")
     towers_of_hanoi(n - 1, auxiliary, source, destination)
-
-# Main Program
 n = int(input("Enter the number of disks: "))
 print("Steps to solve the Towers of Hanoi problem:")
 towers_of_hanoi(n, 'A', 'B', 'C')
+
+
+
+output:
+Move disk 1 from A → C
+Move disk 2 from A → B
+Move disk 1 from C → B
+Move disk 3 from A → C
+Move disk 1 from B → A
+Move disk 2 from B → C
+Move disk 1 from A → C
